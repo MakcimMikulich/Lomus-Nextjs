7a1ffcbeaba2ab093fbec5878cc036d6db881754
Makcik303123 <adoberoflit@gmail.com>
2024-09-05 00:11:36 +0300

    add modal

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@ import styles from "./page.module.scss";
 
 import { Header } from "./ui";
 import { Advantages, Preview, Select } from "./sections";
+import { useState } from "react";
 
 export default function Home() {
-	return (
-		<>
-			<Header />
-			<main className={styles.main}>
-				<Preview />
-				<Advantages />
-				<Select />
-			</main>
-		</>
-	);
+    // const [modalOpen, setModalOpen] = useState()
+    return (
+        <>
+            <Header />
+            <main className={styles.main}>
+                <Preview />
+                <Advantages />
+                <Select />
+            </main>
+        </>
+    );
 }
--- app/ui/header/Header.tsx
+++ app/ui/header/Header.tsx
@@ -6,35 +6,35 @@ import styles from "./Header.module.scss";
 import { scrollToElement } from "@/app/libs/scrollToElement";
 
 const Header = () => {
-	const handlerLogo = () => {
-		const element = document.body;
+    const handlerLogo = () => {
+        const element = document.body;
 
-		if (element instanceof HTMLElement) {
-			scrollToElement(element);
-		}
-	};
+        if (element instanceof HTMLElement) {
+            scrollToElement(element);
+        }
+    };
 
-	const handlerBtn = () => {
-		const element = document.querySelector("#target");
+    const handlerBtn = () => {
+        const element = document.querySelector("#target");
 
-		if (element instanceof HTMLElement) {
-			scrollToElement(element);
-		}
-	};
+        if (element instanceof HTMLElement) {
+            scrollToElement(element);
+        }
+    };
 
-	return (
-		<div className={styles.header_wrapper}>
-			<header className={styles.header}>
-				<div onClick={handlerLogo} className={styles.logo_wrapper}>
-					<img src={logo.src} alt={"logo"} />
-					<span>VPN</span>
-				</div>
-				<button onClick={handlerBtn} className={styles.button}>
-					Get VPN
-				</button>
-			</header>
-		</div>
-	);
+    return (
+        <div className={styles.header_wrapper}>
+            <header className={styles.header}>
+                <div onClick={handlerLogo} className={styles.logo_wrapper}>
+                    <img src={logo.src} alt={"logo"} />
+                    <span>VPN</span>
+                </div>
+                <button onClick={handlerBtn} className={styles.button}>
+                    Get VPN
+                </button>
+            </header>
+        </div>
+    );
 };
 
 export { Header };
